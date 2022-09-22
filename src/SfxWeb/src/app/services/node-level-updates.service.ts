0f9ac711c71017333dfbd85f3457c02428c8d8f5
Roman Khotsyn <rokhot@microsoft.com>
2022-09-22 14:53:26 -0700

    Add Guest/Host information

--- src/SfxWeb/src/app/services/node-level-updates.service.ts
+++ src/SfxWeb/src/app/services/node-level-updates.service.ts
@@ -2,6 +2,7 @@ import { ElementRef, Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { of, Observable, forkJoin } from 'rxjs';
 import { mergeMap, map, finalize, catchError, groupBy } from 'rxjs/operators';
+import { RepairTask } from '../Models/DataModels/repairTask';
 
 export enum NodeUpdateStatus {
     Ok = 'Ok',
@@ -11,7 +12,8 @@ export enum NodeUpdateStatus {
 
 export interface INodeUpdateStatus {
     nodeName: string;
-    status: NodeUpdateStatus;
+    guestStatus: NodeUpdateStatus;
+    hostStatus: NodeUpdateStatus;
 }
 
 @Injectable({
@@ -19,10 +21,13 @@ export interface INodeUpdateStatus {
 })
 export class NodeLevelUpdatesService {
     public containerRef: ElementRef;
-    
+
     constructor(private data: DataService) {}
 
     public getNodeStatus() : Observable<INodeUpdateStatus[]> {
+        const GuestRepairActions = ["TenantInitiatedGuestUpdate", "TenantInitiatedGuestAndHostUpdate"];
+        const HostRepairActions = ["TenantInitiatedHostUpdate", "TenantInitiatedGuestAndHostUpdate"];
+
         return forkJoin({
             nodes: this.data.nodes.ensureInitialized(),
             pendingUpdates: this.data.infrastructurePendingUpdatesCollection.ensureInitialized(),
@@ -33,27 +38,37 @@ export class NodeLevelUpdatesService {
             const pendingUpdates = this.data.infrastructurePendingUpdatesCollection.collection;
             const repairTasks = this.data.repairCollection.collection;
 
-            const nodeLevelRepairs = repairTasks.filter(r => r.raw.Action.startsWith("TenantInitiated"));
+            const nodeLevelRepairs = repairTasks.filter(r => GuestRepairActions.includes(r.raw.Action)
+                                                          || HostRepairActions.includes(r.raw.Action));
 
-            return nodes.map(node => {            
-                const lastRepair = nodeLevelRepairs
-                    .filter(r => r.raw.Target?.NodeNames.indexOf(node.name) !== -1)
-                    .filter(r => r.raw.History?.CreatedUtcTimestamp)
-                    .sort((a, b) =>  {
-                        const aCreatedDate = new Date(a.raw.History?.CreatedUtcTimestamp ?? 0);
-                        const bCreatedDate = new Date(b.raw.History?.CreatedUtcTimestamp ?? 0);
+            return nodes.map(node => {       
+                function compareRepairCreateDate(a: RepairTask, b: RepairTask): number {
+                    const aCreatedDate = new Date(a.raw.History?.CreatedUtcTimestamp ?? 0);
+                    const bCreatedDate = new Date(b.raw.History?.CreatedUtcTimestamp ?? 0);
 
-                        return bCreatedDate.getTime() - aCreatedDate.getTime();
-                    })
+                    return bCreatedDate.getTime() - aCreatedDate.getTime();
+                }
+
+                const nodeNLURepairs = nodeLevelRepairs
+                    .filter(r => r.raw.Target?.NodeNames.indexOf(node.name) !== -1);
+                const lastGuestRepair = nodeNLURepairs
+                    .filter(r => GuestRepairActions.includes(r.raw.Action))
+                    .sort(compareRepairCreateDate)
                     .shift();
-                const isInProgress = lastRepair && lastRepair.raw.State !== "Completed";
-                const isPending = pendingUpdates.some(pu => pu.raw.RoleInstanceName.replace("_IN_", '.') === node.name);
+                const lastHostRepair = nodeNLURepairs
+                    .filter(r => HostRepairActions.includes(r.raw.Action))
+                    .sort(compareRepairCreateDate)
+                    .shift();
+                const isGuestInProgress = lastGuestRepair && lastGuestRepair.raw.State !== "Completed";
+                const isHostInProgress = lastHostRepair && lastHostRepair.raw.State !== "Completed";
 
-                const nodeStatus = isInProgress ? NodeUpdateStatus.InProgress
-                                                : (isPending ? NodeUpdateStatus.Pending : NodeUpdateStatus.Ok);
+                const isGuestPending = pendingUpdates.some(pu => pu.raw.RoleInstanceName.replace("_IN_", '.') === node.name);
+
                 return {
                     nodeName: node.name,
-                    status: nodeStatus,
+                    guestStatus: isGuestInProgress ? NodeUpdateStatus.InProgress
+                                                   : (isGuestPending ? NodeUpdateStatus.Pending : NodeUpdateStatus.Ok),
+                    hostStatus: isHostInProgress ? NodeUpdateStatus.InProgress : NodeUpdateStatus.Ok,
                 };
             });
         }));        
